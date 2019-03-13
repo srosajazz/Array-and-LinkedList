742094a262ee20b1db61bd254f178218a12271fa
Sergio Rosa <scarlosjazz@gmail.com>
2019-03-13 18:18:37 -0400

    LinkedList

---
+++ Array.js
@@ -0,0 +1,28 @@
+//===============Implemeting Array===============
+class MArray{
+    //Cosntructor
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
+    //get()
+    //access the data
+    get(index){
+        return this.data[index];
+    }; 
+    
+    //push()
+    push(item){};
+
+    //pop()
+    pop(){};
+
+    //delete()
+    delete(){};
+
+    // shift all numbers
+    shifItems(index){};
+}
+
+const newArray = new MArray();
+console.log(newArray);
---
+++ linkedlist.js
@@ -0,0 +1,59 @@
+// 10-->5-->16 ->null
+
+class Node {
+    construction(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  class LinkedList{
+    constructor(value){
+      this.head = {
+        value: value,
+        next: null,
+      }
+      this.tail = this.head;
+      this.length = 1;
+    }
+  
+    //append()
+    append(value){
+      //instantiated  - ES6
+      const newNode = new Node(value);
+      //1.create the node
+      // const newNode = {
+      //   value: value,
+      //   next: null,
+      // };
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+      return this;
+    }
+    //prepend() Add (1)-->10-->5-->16 ->null
+    prepend(value){
+      //1.create the node
+      const newNode = {
+        value: value,
+        next: null,
+      };
+      //create a pointer
+      newNode.next = this.head;
+      //updated the reference
+      this.head = newNode;
+      this.length++;
+      return this;
+    }
+  }
+  
+  //out put Undefined
+  const myLinkedList = new LinkedList(10);
+  
+  
+  //command
+  myLinkedList.append(5);
+  myLinkedList.append(16);
+  myLinkedList.prepend(1);
+  
+  //log out all data and length from the LL 
+  //console.log(myLinkedList);
